451a6a8b5398f68c5ce751f5feb4e07d2f660fb5
Simone Amadori <simone@amadori.bs.it>
2025-11-11 23:46:50 +0100

    1.7c

--- batchcode_plugin/plugin.py
+++ batchcode_plugin/plugin.py
@@ -24,72 +24,72 @@ class BatchCodePlugin(InvenTreePlugin, SettingsMixin):
     VERSION = "1.7"
 
     SETTINGS = {
-        "target_field": {
-            "name": _("Target Field"),
-            "description": _("Campo del modello StockItem dove salvare il codice batch"),
-            "default": "batch",
-            "required": True,
-            "type": "str",
+        'target_field': {
+            'name': _('Target Field'),
+            'description': _('Campo del modello StockItem dove salvare il codice batch'),
+            'default': 'batch',
+            'required': True,
+            'type': 'str',
         },
-        "code_format": {
-            "name": _("Code Format"),
-            "description": _("Formato del codice batch, usare {num} per il numero progressivo e {loc} per il prefisso location"),
-            "default": "B{num:06d}",
-            "required": True,
-            "type": "str",
+        'code_format': {
+            'name': _('Code Format'),
+            'description': _('Formato del codice batch, usare {num} per il numero progressivo e {loc} per il prefisso location'),
+            'default': 'B{num:06d}',
+            'required': True,
+            'type': 'str',
         },
-        "trigger": {
-            "name": _("Trigger"),
-            "description": _("Quando generare il codice: 'always', 'manual'"),
-            "default": "always",
-            "required": True,
-            "type": "choice",
-            "choices": ["always", "manual"],
+        'trigger': {
+            'name': _('Trigger'),
+            'description': _('Quando generare il codice: \'always\', \'manual\''),
+            'default': 'always',
+            'required': True,
+            'type': 'choice',
+            'choices': ['always', 'manual'],
         },
-        "use_location_prefix": {
-            "name": _("Use Location Prefix"),
-            "description": _("Se attivo, aggiunge il prefisso della location al codice batch"),
-            "default": False,
-            "required": False,
-            "type": "bool",
+        'use_location_prefix': {
+            'name': _('Use Location Prefix'),
+            'description': _('Se attivo, aggiunge il prefisso della location al codice batch'),
+            'default': False,
+            'required': False,
+            'type': 'bool',
         },
-        "location_field": {
-            "name": _("Location Field"),
-            "description": _("Nome del campo location per generare il prefisso"),
-            "default": "location",
-            "required": False,
-            "type": "str",
+        'location_field': {
+            'name': _('Location Field'),
+            'description': _('Nome del campo location per generare il prefisso'),
+            'default': 'location',
+            'required': False,
+            'type': 'str',
         },
-        "reset_daily": {
-            "name": _("Reset Daily"),
-            "description": _("Se attivo, il contatore si resetta ogni giorno"),
-            "default": False,
-            "required": False,
-            "type": "bool",
-        }
+        'reset_daily': {
+            'name': _('Reset Daily'),
+            'description': _('Se attivo, il contatore si resetta ogni giorno'),
+            'default': False,
+            'required': False,
+            'type': 'bool',
+        },
     }
 
     def register_events(self):
-        trigger = self.get_setting("trigger") or "always"
-        if trigger == "always":
-            return {"stockitem.created": self.on_stockitem_created}
+        trigger = self.get_setting('trigger') or 'always'
+        if trigger == 'always':
+            return {'stockitem.created': self.on_stockitem_created}
         return {}
 
     def on_stockitem_created(self, sender, instance, **kwargs):
-        target_field = self.get_setting("target_field") or "batch"
-        code_format = self.get_setting("code_format") or "B{num:06d}"
-        use_loc_prefix = self.get_setting("use_location_prefix") or False
-        location_field = self.get_setting("location_field") or "location"
+        target_field = self.get_setting('target_field') or 'batch'
+        code_format = self.get_setting('code_format') or 'B{num:06d}'
+        use_loc_prefix = self.get_setting('use_location_prefix') or False
+        location_field = self.get_setting('location_field') or 'location'
 
         current_value = getattr(instance, target_field, None)
         if current_value:
-            logger.info(f"[v1.7] StockItem {instance.pk} già ha batch {current_value}")
+            logger.info(f'[v1.7] StockItem {instance.pk} già ha batch {current_value}')
             return
 
         # Recupera ultimo codice
-        queryset = StockItem.objects.exclude(**{f"{target_field}__isnull": True}).exclude(**{f"{target_field}": ""})
+        queryset = StockItem.objects.exclude(**{f'{target_field}__isnull': True}).exclude(**{f'{target_field}': ''})
         last_entry = queryset.aggregate(Max(target_field))
-        last_code = last_entry[f"{target_field}__max"]
+        last_code = last_entry[f'{target_field}__max']
 
         try:
             if last_code:
@@ -100,14 +100,14 @@ class BatchCodePlugin(InvenTreePlugin, SettingsMixin):
         except (TypeError, ValueError):
             new_number = 1
 
-        prefix = ""
+        prefix = ''
         if use_loc_prefix and hasattr(instance, location_field):
             loc = getattr(instance, location_field)
             if loc:
-                prefix = str(loc).upper() + "-"
+                prefix = str(loc).upper() + '-'
 
-        new_batch = f"{prefix}{code_format.format(num=new_number, loc=prefix)}"
+        new_batch = f'{prefix}{code_format.format(num=new_number, loc=prefix)}'
         setattr(instance, target_field, new_batch)
         instance.save()
 
-        logger.info(f"[v1.7] Generato batch {new_batch} per StockItem {instance.pk}")
+        logger.info(f'[v1.7] Generato batch {new_batch} per StockItem {instance.pk}')
