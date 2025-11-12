54d77de315a789c6828302c2d4e757a2a132f20f
Simone Amadori <simone@amadori.bs.it>
2025-11-12 23:06:22 +0100

    versione compatibile 1.1.3

--- batchcode_plugin/plugin.py
+++ batchcode_plugin/plugin.py
@@ -1,113 +1,144 @@
-# src/plugins/batchcode_plugin/plugin.py
-
 from plugin import InvenTreePlugin
 from plugin.mixins import SettingsMixin
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from stock.models import StockItem
 from django.db.models import Max
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.utils.translation import gettext_lazy as _
+import datetime
 import logging
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger("inventree")
 
-class BatchCodePlugin(InvenTreePlugin, SettingsMixin):
+
+class BatchCodePlugin(SettingsMixin, InvenTreePlugin):
     """
-    BatchCodePlugin v1.7
-    Genera codici batch progressivi con tutte le opzioni.
+    Plugin per generare automaticamente un codice batch univoco e progressivo.
+    Compatibile con InvenTree 1.1.3 (usa signals Django, non EventMixin).
     """
 
     AUTHOR = "Simone Amadori"
     WEBSITE = "https://github.com/Kamaar"
     NAME = "BatchCodePlugin"
-    SLUG = "batchcode"  # Deve essere unico e tutto minuscolo
-    TITLE = _("Batch Code Generator")
-    DESCRIPTION = _("Genera automaticamente codici batch numerici progressivi per ogni nuovo StockItem.")
-    VERSION = "1.7"
+    SLUG = "batchcode"
+    TITLE = "Batch Code Generator"
+    DESCRIPTION = "Genera automaticamente codici batch numerici progressivi per ogni nuovo StockItem."
+    VERSION = "1.1.3-compatible"
 
     SETTINGS = {
-        'target_field': {
-            'name': _('Target Field'),
-            'description': _('Campo del modello StockItem dove salvare il codice batch'),
-            'default': 'batch',
-            'required': True,
-            'type': 'str',
+        "TARGET_FIELD": {
+            "name": _("Target Field"),
+            "description": _("Campo dello StockItem dove salvare il codice (es. 'batch', 'serial' o campo custom)"),
+            "default": "batch",
         },
-        'code_format': {
-            'name': _('Code Format'),
-            'description': _('Formato del codice batch, usare {num} per il numero progressivo e {loc} per il prefisso location'),
-            'default': 'B{num:06d}',
-            'required': True,
-            'type': 'str',
+        "CODE_FORMAT": {
+            "name": _("Code Format"),
+            "description": _("Formato del codice batch. Placeholders: {prefix}, {num}, {date}, {part}, {loc}. Es.: {prefix}{date:%Y%m%d}-{num:04d}"),
+            "default": "{prefix}{date:%Y%m%d}-{num:04d}",
         },
-        'trigger': {
-            'name': _('Trigger'),
-            'description': _('Quando generare il codice: \'always\', \'manual\''),
-            'default': 'always',
-            'required': True,
-            'type': 'choice',
-            'choices': ['always', 'manual'],
+        "PREFIX": {
+            "name": _("Prefix"),
+            "description": _("Prefisso statico da usare se non si usa il prefisso location."),
+            "default": "B",
         },
-        'use_location_prefix': {
-            'name': _('Use Location Prefix'),
-            'description': _('Se attivo, aggiunge il prefisso della location al codice batch'),
-            'default': False,
-            'required': False,
-            'type': 'bool',
+        "MIN_DIGITS": {
+            "name": _("Min digits"),
+            "description": _("Numero minimo di cifre per la parte numerica del codice."),
+            "default": 4,
+            "validator": [int, MinValueValidator(1), MaxValueValidator(12)],
         },
-        'location_field': {
-            'name': _('Location Field'),
-            'description': _('Nome del campo location per generare il prefisso'),
-            'default': 'location',
-            'required': False,
-            'type': 'str',
+        "DAILY_RESET": {
+            "name": _("Reset giornaliero"),
+            "description": _("Se attivo, il contatore si azzera ogni giorno (basato sulla data di creazione)."),
+            "validator": bool,
+            "default": False,
         },
-        'reset_daily': {
-            'name': _('Reset Daily'),
-            'description': _('Se attivo, il contatore si resetta ogni giorno'),
-            'default': False,
-            'required': False,
-            'type': 'bool',
+        "PER_PART": {
+            "name": _("Progressivo per parte"),
+            "description": _("Se attivo, ogni Part (instance.part) avrà il suo contatore separato."),
+            "validator": bool,
+            "default": False,
+        },
+        "USE_LOCATION_PREFIX": {
+            "name": _("Use Location Prefix"),
+            "description": _("Se attivo, usa un valore della StockLocation come prefisso (es. name o code)."),
+            "validator": bool,
+            "default": False,
+        },
+        "LOCATION_FIELD": {
+            "name": _("Location Field"),
+            "description": _("Campo della StockLocation da usare come prefisso (es. 'name' o 'code')."),
+            "default": "name",
+        },
+        "ENABLED": {
+            "name": _("Enabled"),
+            "description": _("Abilita o disabilita la generazione automatica dei codici batch."),
+            "validator": bool,
+            "default": True,
         },
     }
 
-    def register_events(self):
-        trigger = self.get_setting('trigger') or 'always'
-        if trigger == 'always':
-            return {'stockitem.created': self.on_stockitem_created}
-        return {}
 
-    def on_stockitem_created(self, sender, instance, **kwargs):
-        target_field = self.get_setting('target_field') or 'batch'
-        code_format = self.get_setting('code_format') or 'B{num:06d}'
-        use_loc_prefix = self.get_setting('use_location_prefix') or False
-        location_field = self.get_setting('location_field') or 'location'
-
-        current_value = getattr(instance, target_field, None)
-        if current_value:
-            logger.info(f'[v1.7] StockItem {instance.pk} già ha batch {current_value}')
+# --- Segnale Django: post_save di StockItem ---
+@receiver(post_save, sender=StockItem)
+def assign_batch_code(sender, instance, created, **kwargs):
+    """
+    Genera automaticamente il codice batch alla creazione di uno StockItem.
+    """
+    try:
+        plugin = BatchCodePlugin()
+        if not plugin.get_setting("ENABLED", True):
             return
 
-        # Recupera ultimo codice
-        queryset = StockItem.objects.exclude(**{f'{target_field}__isnull': True}).exclude(**{f'{target_field}': ''})
-        last_entry = queryset.aggregate(Max(target_field))
-        last_code = last_entry[f'{target_field}__max']
+        if not created:
+            return
 
-        try:
-            if last_code:
-                number_part = ''.join(filter(str.isdigit, str(last_code)))
-                new_number = int(number_part) + 1
-            else:
-                new_number = 1
-        except (TypeError, ValueError):
-            new_number = 1
+        target_field = plugin.get_setting("TARGET_FIELD", "batch")
+        if getattr(instance, target_field, None):
+            return
 
-        prefix = ''
-        if use_loc_prefix and hasattr(instance, location_field):
-            loc = getattr(instance, location_field)
-            if loc:
-                prefix = str(loc).upper() + '-'
+        prefix = plugin.get_setting("PREFIX", "B")
+        min_digits = plugin.get_setting("MIN_DIGITS", 4)
+        daily_reset = plugin.get_setting("DAILY_RESET", False)
+        per_part = plugin.get_setting("PER_PART", False)
+        code_format = plugin.get_setting("CODE_FORMAT", "{prefix}{date:%Y%m%d}-{num:04d}")
 
-        new_batch = f'{prefix}{code_format.format(num=new_number, loc=prefix)}'
-        setattr(instance, target_field, new_batch)
+        # Base queryset per trovare ultimo batch
+        queryset = StockItem.objects.exclude(**{f"{target_field}__isnull": True}).exclude(**{target_field: ""})
+        if per_part and instance.part:
+            queryset = queryset.filter(part=instance.part)
+        if daily_reset:
+            today = datetime.date.today()
+            queryset = queryset.filter(creation_date__date=today)
+
+        last_batch = queryset.aggregate(Max(target_field))
+        last_code = last_batch[f"{target_field}__max"]
+
+        # Estrai numero progressivo
+        new_number = 1
+        if last_code:
+            import re
+            digits = re.findall(r"(\d+)$", str(last_code))
+            if digits:
+                new_number = int(digits[-1]) + 1
+
+        date = datetime.date.today()
+        loc = instance.location.name if instance.location else ""
+        part = instance.part.name if instance.part else ""
+
+        batch_code = code_format.format(
+            prefix=prefix,
+            num=new_number,
+            date=date,
+            loc=loc,
+            part=part,
+        )
+
+        setattr(instance, target_field, batch_code)
         instance.save()
 
-        logger.info(f'[v1.7] Generato batch {new_batch} per StockItem {instance.pk}')
+        logger.info(f"[BatchCodePlugin] Assegnato batch code {batch_code} a StockItem {instance.pk}")
+
+    except Exception as e:
+        logger.error(f"[BatchCodePlugin] Errore nella generazione del batch: {e}")
